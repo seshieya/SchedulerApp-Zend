f241e4bbc2fd2d010daa93c5979d05cfe36cc2db
Aela <hm.angela.wu@gmail.com>
2017-09-06 23:43:12 -0700

    Use AJAX to call PHP function to regenerate dates on Draft Schedule page

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -44,6 +44,16 @@ return [
                     ],
                 ],
             ],
+            'generateDates' => [
+                'type' => Literal::class,
+                'options' => [
+                    'route'    => '/schedule/dates',
+                    'defaults' => [
+                        'controller' => Controller\ScheduleController::class,
+                        'action'     => 'dates',
+                    ],
+                ],
+            ],
             'login' => [
                 'type' => Literal::class,
                 'options' => [
@@ -169,6 +179,7 @@ return [
     'view_manager' => [
         'display_not_found_reason' => true,
         'display_exceptions'       => true,
+        'strategies'               => ['ViewJsonStrategy'],
         'doctype'                  => 'HTML5',
         'not_found_template'       => 'error/404',
         'exception_template'       => 'error/index',
